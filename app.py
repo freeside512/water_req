57afabce7d91563841fd86c3139ce9c18811f339
freeside512 <freeside512@gmail.com>
2024-12-20 19:02:11 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from weather import Weather
 
@@ -21,4 +22,5 @@ def get_weather():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
