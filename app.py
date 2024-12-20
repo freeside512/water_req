a028c66e8b2bc1e8fa4280965d9c9cc0d1bd1d41
freeside512 <freeside512@gmail.com>
2024-12-20 20:59:38 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask, request, jsonify
+import joblib
 from weather import Weather
 
 app = Flask(__name__)
+
+
+crop_model = joblib.load('data/crop_rec.joblib')
+water_model = joblib.load('data/water_level_pl.joblib')
+
 
 @app.route('/weather', methods=['POST'])
 def get_weather():
@@ -15,15 +21,45 @@ def get_weather():
 
     weather = Weather(longitude=longitude, latitude=latitude)
     weather.temp_cond()
-    
+
     return jsonify({
         'temperature': weather.temp,
         'condition': weather.condition,
-        'icon':weather.icon,
-        'send_condition':weather.send_cond,
-        'send_temp':weather.send_temp,
-        'send_region':weather.send_region
+        'icon': weather.icon,
+        'send_condition': weather.send_cond,
+        'send_temp': weather.send_temp,
+        'send_region': weather.send_region
     })
+
+
+@app.route('/predict_crop', methods=['POST'])
+def predict_crop():
+    data = request.json
+    features = data.get('features')
+
+    if not features:
+        return jsonify({'error': 'Features are required'}), 400
+
+    try:
+        prediction = crop_model.predict([features])
+        return jsonify({'crop_prediction': prediction[0]})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/predict_water', methods=['POST'])
+def predict_water():
+    data = request.json
+    features = data.get('features')
+
+    if not features:
+        return jsonify({'error': 'Features are required'}), 400
+
+    try:
+        prediction = water_model.predict([features])
+        return jsonify({'water_level_prediction': prediction[0]})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
